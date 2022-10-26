d9060c09bb124d635f81f3c6f8dbd9790b336f57
Ronald Garcia <ronaldag58@gmail.com>
2022-10-26 15:30:00 -0300

    feat: add json parse

--- Consumindo-API/src/App.java
+++ Consumindo-API/src/App.java
@@ -1,13 +1,11 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-
 public class App {
     public static void main(String[] args) throws Exception {
         //Get uri
-        String imdb250Movies = "https://imdb-api.com/en/API/Top250Movies/your_key";
+        String imdb250Movies = "https://imdb-api.com/en/API/Top250Movies/k_f360sfvm";
         var link = URI.create(imdb250Movies);
 
         // create a client
@@ -20,6 +18,10 @@ public class App {
 
         //use the client to send the request and print the body
         var response = client.send(request, BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        //Parse Json
+        String jsonMoviesFull = response.body();
+        String[] jsonMovies = jsonMoviesFull.split("\\},\\{");
+        System.out.println(jsonMovies[2]);
     }
 }
